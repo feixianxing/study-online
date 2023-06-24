19c9678b3dc21bcf50c7a7d02296be52f3024feb
Tangfc <1737442449@qq.com>
2023-06-24 10:29:41 +0800

    Test

---
+++ src/views/TestView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <div>
+            <ul>
+                <li v-for="item in 7">{{ item }} test</li>
+            </ul>
+        </div>
+        <div>
+            test
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>
